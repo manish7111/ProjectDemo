34241907ca53bc3d2e76752a175e06f136e00aff
Manish Reddy <manishreddy@Manishs-MacBook-Air.local>
2023-07-10 11:03:18 +0530

    [Manish] Add: Folder structure created.

---
+++ ProjectDemo/ProjectDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
